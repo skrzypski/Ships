aa18141e847052390d9264085f47bddcf8af6455
Kacper Skrzypek <kac.pek.3@hotmail.com>
2020-02-26 20:50:26 +0100

    main window finished

--- Ships/AboutWindow.xaml.cs
+++ Ships/AboutWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Ships
 {
     /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
+    /// Logika interakcji dla klasy AboutWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AboutWindow : Window
     {
-        public MainWindow()
+        public AboutWindow()
         {
             InitializeComponent();
-        }
-
-        private void aboutTb_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            new AboutWindow().Show();
-        }
-
-        private void startBtn_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
